312c296c65189f4f38d651a7cd3c2225e426eb21
Ihorchuk <ihor.sumenkov.ir.2023@lpnu.ua>
2023-11-22 14:00:32 +0200

    add first python lab code

---
+++ lab1_part2.py
@@ -0,0 +1,25 @@
+# Присвоюєм змінній fruit значення свого улюбленого фрукта та виводим рядок
+fruit = "Apple"
+print (fruit)
+
+# Присвоюєм змінній score мій бал з математики
+score = 90
+# Тепер присвоюєм змінній result змінну та прирівнюєм її до числа 60 і виводим рядок
+result = score == 60
+print (result)
+# Рядок має вивсети False так як 90 != 60
+
+# Присвоюєм змінним "is_even" та "is_odd" значення "False" та "True"
+is_even = False
+is_odd = True
+
+#Тут виводим результати логічних операцій тобто "and, or, not"
+#
+print(is_even and is_odd)
+print(is_even or is_odd)
+print(not is_even)
+print(not is_odd)
+#Виходить що результат print(is_even and is_odd) має бути False так як один із них = False
+#print(is_even or is_odd) має бути True так як одна зі змінних має False
+#print(not is_even) має бути True так як сама змінна False
+#print(not is_odd) має бути False так як сама змінна True
